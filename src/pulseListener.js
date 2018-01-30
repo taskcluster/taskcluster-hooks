8e991e4f1bc4e76c5cad8f5985e97d4ac404f2b1
Alexandra da Silva <alesilva241@gmail.com>
2018-01-30 19:54:50 -0200

    durable Queue

--- src/pulseListener.js
+++ src/pulseListener.js
@@ -1,13 +1,12 @@
 var taskcluster = require('taskcluster-client');
-var queuePulses = new taskcluster.QueueEvents();
 
 options = {
   pulseUser: process.env.PULSE_USERNAME,
   pulsePass: process.env.PULSE_PASSWORD,
+  newqueueName : 'pulsedMessages',
   exchange: 'exchange/taskcluster-queue/v1/task-running',
-  routingKey: '#',
-  durableQueue: queuePulses
-}
+  routingKey: '#'
+};
 
 var connection = new taskcluster.PulseConnection({
   username: options.pulseUser,
@@ -16,11 +15,7 @@ var connection = new taskcluster.PulseConnection({
 
 var listener = new taskcluster.PulseListener({
   connection: connection,
-  credentials: {
-    username: options.pulseUser,
-    password: options.password
-  },
-  queueName: options.durableQueue,
+  queueName: options.newqueueName
 });
 
 
@@ -29,11 +24,9 @@ listener.bind({
   routingKeyPattern: options.routingKey
 });
 
-console.log('Queue', queuePulses);
-
 listener.on('message', (msg) => {
   queuePulses = msg;
-  //console.log(queuePulses);
+  console.log(queuePulses);
 });
 
  listener.resume();
