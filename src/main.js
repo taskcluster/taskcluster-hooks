3bb6389f894b5171a7cec898358976e9b56e020d
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-02-01 19:26:58 -0800

    Use same table prefix for metrics as other places

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ var load = loader({
     setup: ({cfg, process, monitor}) => {
       return data.Hook.setup(_.defaults({
         table:        cfg.app.hookTable,
-        monitor:      monitor.prefix('data.hooks'),
+        monitor:      monitor.prefix('table.hooks'),
       }, cfg.azureTable, cfg.taskcluster));
     },
   },
