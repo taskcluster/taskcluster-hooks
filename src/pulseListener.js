c8b3ef0cc8c58d51111b9f5ce76bdc7b9f213c7c
Alexandra da Silva <alesilva241@gmail.com>
2018-02-05 21:29:42 -0200

    listening and consuming pulse messages

--- src/pulseListener.js
+++ src/pulseListener.js
@@ -1,31 +1,49 @@
 var taskcluster = require('taskcluster-client');
+//var hookApi = require('./v1');
 
 options = {
   pulseUser: process.env.PULSE_USERNAME,
   pulsePass: process.env.PULSE_PASSWORD,
   newqueueName : 'pulsedMessages',
-  exchange: 'exchange/taskcluster-queue/v1/task-running',
-  routingKey: '#'
+  exchange: 'exchange/taskcluster-queue/v1/task-completed',
+  routingKey: '#',
 };
 
 var connection = new taskcluster.PulseConnection({
   username: options.pulseUser,
-  password: options.pulsePass
+  password: options.pulsePass,
 });
 
 var listener = new taskcluster.PulseListener({
   connection: connection,
-  queueName: options.newqueueName
+  queueName: options.newqueueName,
 });
-
 
 listener.bind({
-  exchange: options.exchange, 
-  routingKeyPattern: options.routingKey
+  exchange: 'exchange/taskcluster-queue/v1/task-completed', 
+  routingKeyPattern: options.routingKey,
 });
 
-listener.on('message', (msg) => {
-  console.log(pulsedMessages)
+listener.on('message', (message) => {
+  try {
+    console.log(message);
+  } catch (err) {
+    reject(err);
+  }
 });
 
- await listener.resume();
+listener.connect().then(function(connection) {
+
+  return connection.createChannel().then(function (channel) {
+      
+    var ok = channel.assertQueue(options.newqueueName, {duranle:true});
+    console.log(ok);
+    ok = ok.then(function(queueOk) {
+      return channel.consume(options.newqueueName, function (message) {
+        
+      });  
+    });
+  })
+}).catch(console.warn);
+
+listener.resume();
