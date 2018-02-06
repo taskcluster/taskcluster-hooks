a1a1460664d7443cb12acc28dc5257af9df96897
Alexandra da Silva <alesilva241@gmail.com>
2018-02-06 00:26:07 -0200

    also update the pulseMessage when editing

--- src/v1.js
+++ src/v1.js
@@ -328,6 +328,7 @@ api.declare({
     hook.schedule          = schedule;
     hook.nextTaskId        = taskcluster.slugid();
     hook.nextScheduledDate = nextDate(schedule);
+    hook.pulseExchanges    = hookDef.pulseExchanges;
   });
 
   let definition = await hook.definition();
